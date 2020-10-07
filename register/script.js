f00c52c433c7bf7449f7bfc6a96c77a4159d9316
LCamile <70186080+LCamile@users.noreply.github.com>
2020-10-08 01:09:29 +0800

    Create script.js

---
+++ register/script.js
@@ -0,0 +1,10 @@
+function Alert(){
+if(document.getElementById('cbxPolicy2').checked == true){
+  alert('You are now registered for Game');
+}
+else{
+  alert('Please check the "I accept terms and condition"');
+ event.preventDefault();
+}
+
+}
